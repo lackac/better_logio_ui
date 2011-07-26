299697161316789f9c2392dc516b2d86dd67c8aa
Laszlo Bacsi <lackac@lackac.hu>
2011-07-26 11:36:53 +0200

    ANSI colors in streams and default white text color

---
+++ better_logio.js
@@ -0,0 +1,42 @@
+var BetterLogIo = function() {
+  function ansi2html(text) {
+    var openTags = 0;
+    var converted = text.replace(/\[(\d\d?)m/g, function(m, code) {
+      if (code === "0") {
+        var closingTags = "";
+        while (openTags--) closingTags += "</span>";
+        return closingTags;
+      } else {
+        openTags += 1;
+        return '<span class="ansi-'+code+'">';
+      }
+    });
+    if (openTags > 0) {
+      var closingTags = "";
+      while (openTags--) closingTags += "</span>";
+      converted += closingTags;
+    }
+    return converted;
+  }
+
+  Stream.prototype.log = function(log_file, msg) {
+    if (!this._paused) {
+      msg = msg.replace(/</ig,"&lt;").replace(/>/ig,"&gt;");
+      msg = ansi2html(msg);
+      var html = $('<p><span class="label labelcolor'
+        + log_file.color + '">' + log_file.node.label + ':'
+        + log_file.label + '</span> ' + msg + '</p>');
+      this._dom.find(".console").prepend(html);
+    }
+    // Apply highlights if enabled
+    if (this._searcher && this.highlight) {
+      this._searcher.highlight(html);
+      this._searcher.update_match_count();
+    }
+  }
+}
+
+var script = document.createElement("script");
+script.type = 'text/javascript';
+script.textContent = "("+BetterLogIo.toString()+")();";
+document.body.appendChild(script);
